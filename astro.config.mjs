88e08b25b4f110d1eb93f3bb1f7396022ebe6ef7
gabo <gabriel.gaboardi13@gmail.com>
2023-12-29 00:56:20 -0300

    initial tools' docs implementantion

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,36 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: "My Docs",
+      social: {
+        github: "https://github.com/withastro/starlight",
+      },
+      logo: {
+        src: "./src/assets/tinytools_alpha.svg",
+        replacesTitle: true,
+      },
+      customCss: [
+        // Relative path to your custom CSS file
+        "./src/styles/custom.css",
+      ],
+      sidebar: [
+        {
+          label: "Tools",
+          autogenerate: { directory: "guides" },
+        },
+        {
+          label: "Guides",
+          autogenerate: { directory: "reference" },
+        },
+        {
+          label: "Snippets",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+    }),
+  ],
 });
