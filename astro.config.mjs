ae73dd4f061fb4b4eecf41e4808d11f313e0dfbc
gabo <gabriel.gaboardi13@gmail.com>
2024-01-06 02:10:35 -0300

    updated site domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://tinytools.github.io",
+  site: "https://tinytoolsh.github.io/",
   integrations: [
     starlight({
       title: "TinyTools Docs",
